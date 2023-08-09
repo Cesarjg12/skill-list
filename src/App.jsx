4283a474e8f7d8d277dd48d021c9f670b54c2cd1
cesarjg28 <cesarjg12@yahoo.com>
2023-08-09 16:51:45 -0700

    Added the useState part 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './App.css'
 import './NewSkillForm.css'
 import './NewSkillListItem.css'
 import SkillList from './SkillList'
 import NewSkillListItem from './NewSkillListItem'
 
-const skills = [
+
+export default function App() {
+const initialSkills= [
   { name: "HTML", level: 5 },
   { name: "CSS", level: 3 },
   { name: "JavaScript", level: 4 },
   { name: "Python", level: 2 },
 ];
 
-export default function App() {
+const [skills, setSkills] = useState(initialSkills);
   return (
     <>
     <h1>React Dev Skills</h1>
 
     <SkillList skills={skills} />
     <hr></hr>
-    <ul><NewSkillListItem></NewSkillListItem></ul>
+    <NewSkillListItem skills={skills} setSkills={setSkills} />
     </>
   )
 }
